31f4b0c62ff6fe48d8f52406b0fef0269ba2c02a
Salumao Barbosa da Costa <salubcosta@gmail.com>
2024-02-16 23:52:26 -0300

    testanto operadores condicionais

---
+++ pos-dsv-sis-python/01-fundamentos/03-condicionais.py
@@ -0,0 +1,7 @@
+numero1 = input("Informe um número: ")
+numero2 = input("Informe outro número: ")
+
+if numero1 > numero2:
+    print("Primeiro número é maior.")
+else:
+    print("Segundo número é maior.")
