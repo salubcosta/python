ccd4a8b87113f2c47d8ed02e85b584058ec282af
Salumao Barbosa da Costa <salubcosta@gmail.com>
2024-02-16 23:51:33 -0300

    Primeiro arquivo acompanhando professor

---
+++ pos-dsv-sis-python/01-fundamentos/01-intro.py
@@ -0,0 +1,11 @@
+nome = input("Informe seu nome: ")
+sobrenome = input("Informe seu sobrenome: ")
+
+print("Olá",nome,sobrenome)
+
+'''
+Tipos primitivos:
+str | int | float | bool
+'''
+
+# Comentário de linha única
