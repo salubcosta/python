5c955b0e657aefb2f8cd5f2c8642469024a82385
Salumao Barbosa da Costa <salubcosta@gmail.com>
2024-02-16 23:53:48 -0300

    brincando com loop for

---
+++ pos-dsv-sis-python/01-fundamentos/06-loops.py
@@ -0,0 +1,14 @@
+for i in range(5):
+    print(i)
+
+for i in range(0,5,1):
+    print(i)
+else:
+    print("Finalizado")
+
+count = 0
+while count <= 10:
+    print(count)
+    count += 1
+else:
+    print("Fim do loop...")
