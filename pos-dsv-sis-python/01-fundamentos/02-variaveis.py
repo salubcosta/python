a827d998ad444bf436fbc866f15884f8b966600a
Salumao Barbosa da Costa <salubcosta@gmail.com>
2024-02-16 23:51:57 -0300

    testando variaveis e escopo

---
+++ pos-dsv-sis-python/01-fundamentos/02-variaveis.py
@@ -0,0 +1,6 @@
+salario = float(9900)
+idade = int(30)
+nome = str("Salumao")
+brasileiro = bool(True)
+
+print("Nome: %s, salário: %.2f, idade: %d, Brasileiro: %s" % (nome, salario, idade, brasileiro))
