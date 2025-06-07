50e23e7ccd922934926b206abed310f634aecc61
Salumao Barbosa da Costa <salubcosta@gmail.com>
2025-06-07 20:20:29 -0300

    Exercicio que mostrar o antecessor e o sucessor

---
+++ logica-programacao/100-tarefas-faceis/011-antecessor-sucessor.py
@@ -0,0 +1,6 @@
+# 11. Antecessor e Sucessor
+# Crie um programa que exiba o antecessor e o sucessor do número 90.
+number = int(input("Informe um número: "))
+
+print("O Sucessor do número informado é:",(number+1))
+print("O antecessor do número informado é:",(number-1))
