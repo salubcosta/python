94703604c52efcc57c1af8655029c1842e3d2700
Salumao Barbosa da Costa <salubcosta@gmail.com>
2025-06-07 20:21:26 -0300

    Exercicio que calcula a area de um triangulo

---
+++ logica-programacao/100-tarefas-faceis/013-area-do-retangulo.py
@@ -0,0 +1,6 @@
+# 13. Área do Retângulo
+# Crie um programa que calcule a área de um retângulo com base 5 e altura 7.
+base = 5
+altura = 7
+area = base * altura
+print("A área de um triangulo é:",area)
