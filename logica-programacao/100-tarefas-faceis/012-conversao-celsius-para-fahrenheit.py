a16ff5fd74c6e43e9d0b075726a0877b365a939d
Salumao Barbosa da Costa <salubcosta@gmail.com>
2025-06-07 20:20:55 -0300

    Exercicio que faz conversao de celsius para fahrenheit

---
+++ logica-programacao/100-tarefas-faceis/012-conversao-celsius-para-fahrenheit.py
@@ -0,0 +1,6 @@
+# 12. Conversão Celsius para Fahrenheit
+# Crie um programa que converta 26°C para Fahrenheit.
+celsius = float(input("Informe uma temperatura em graus Celsius: "))
+fahrenheit = celsius * 9 / 5 + 32
+
+print("A  temperatura em Fahrenheit é:",fahrenheit)
