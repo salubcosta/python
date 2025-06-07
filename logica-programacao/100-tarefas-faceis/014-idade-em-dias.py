eef9a960050493c54a7825711b5dec66e6adbfec
Salumao Barbosa da Costa <salubcosta@gmail.com>
2025-06-07 20:21:59 -0300

    Exercicio que calcula quantidade de dias que alguem viveu

---
+++ logica-programacao/100-tarefas-faceis/014-idade-em-dias.py
@@ -0,0 +1,6 @@
+# 14. Idade em Dias
+# Crie um programa que calcule quantos dias viveu uma pessoa com 17 anos (desconsidere anos bissextos).
+idade = int(input("Informe sua idade: "))
+quantidade_de_dias = idade * 365
+
+print(f"Você viveu aproximadamente {quantidade_de_dias} dias")
